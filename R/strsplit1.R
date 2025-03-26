c6ecd4f3087fee83ca5ec173c95523dee13d28f6
Amy Gill <gill.signals@gmail.com>
2025-03-26 11:57:29 -0500

    Add license and description; add strsplit1 example function and its documentation.

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,18 @@
+#' Split a string
+#'
+#' @param x string to split (character vector with one element)
+#' @param split delimiter to split on (character vector with one element)
+#'
+#' @returns a character vector with elements matching the original string split by the delimiter
+#' @export
+#'
+#' @examples
+#' x <- "alpha,bravo,charlie,delta"
+#' strsplit1(x, split = ",)
+
+
 strsplit1 <- function(x, split) {
     strsplit(x, split = split)[[1]]
+
+
 }
